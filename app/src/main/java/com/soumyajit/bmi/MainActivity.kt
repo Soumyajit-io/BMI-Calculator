76321177b4642d4fe0eeeabb14018cf6ee868416
Soumyajit-io <soumyajit010805@gmail.com>
2025-07-19 10:50:57 +0530

    initial Commit

---
+++ app/src/main/java/com/soumyajit/bmi/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.soumyajit.bmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.soumyajit.bmi.ui.theme.BMITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BMITheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("HEllo world")
+                }
+            }
+        }
+    }
+}
